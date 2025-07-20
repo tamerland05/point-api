e42f2b2cd1447b2c7ea6d6e4688f924cfc4ac84a
Timur <timurbik@architecton.tech>
2025-07-20 12:39:35 +0300

    fix: handle all

--- point/controllers/payments/payment.py
+++ point/controllers/payments/payment.py
@@ -1,3 +1,6 @@
+import logging
+from uuid import UUID
+
 from point.controllers.base import BaseController
 from point.errors import ErrorCode
 from point.models import Payment
@@ -6,3 +9,22 @@ from point.models import Payment
 class PaymentController(BaseController[Payment]):
     model = Payment
     error_code = ErrorCode.PAYMENT_NOT_FOUND
+
+    @classmethod
+    async def process_payment(cls, payload: str):
+        try:
+            payload = UUID(payload)
+        except ValueError:
+            logging.warn(f"Invalid payment UUID: {payload}")
+            return {"ok": True}
+
+        payment = await PaymentController.get_or_none(id=payload)
+        if payment is None:
+            logging.warn(f"Not fount payment with id: {payload}")
+        elif payment.done:
+            logging.warn(f"Already done payment with id: {payload}")
+        else:
+            payment.done = True
+            await payment.save(update_fields=["done", "updated_at"])
+
+        return None
--- point/routes/bot/webhook.py
+++ point/routes/bot/webhook.py
@@ -1,10 +1,8 @@
-import logging
-from uuid import UUID
-
 from fastapi import Request, APIRouter
 from aiogram.types import Update
 
 from point.controllers import PaymentController
+from point.services import bs
 
 router = APIRouter()
 
@@ -15,26 +13,8 @@ async def telegram_webhook(request: Request):
     update = Update.model_validate(data)
 
     if update.pre_checkout_query is not None:
-        await update.pre_checkout_query.answer(ok=True)
-        return {"ok": True}
-
-    if update.message is None or update.message.successful_payment is None:
-        return {"ok": True}
-
-    payload = update.message.successful_payment.invoice_payload
-    try:
-        payload = UUID(payload)
-    except ValueError:
-        logging.warn(f"Invalid payment UUID: {payload}")
-        return {"ok": True}
-
-    payment = await PaymentController.get_or_none(id=payload)
-    if payment is None:
-        logging.warn(f"Not fount payment with id: {payload}")
-    elif payment.done:
-        logging.warn(f"Already done payment with id: {payload}")
-    else:
-        payment.done = True
-        await payment.save(update_fields=["done", "updated_at"])
+        await update.pre_checkout_query.answer(ok=True).as_(bs.bot)
+    elif not (update.message is None or update.message.successful_payment is None):
+        await PaymentController.process_payment(payload=update.message.successful_payment.invoice_payload)
 
     return {"ok": True}
