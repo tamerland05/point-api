8ce25fa0831fc0569fb9bd60889fe15aa08fbf94
Timur <timurbik@architecton.tech>
2025-07-24 21:46:52 +0300

    fix: public task view

--- point/view/earn/task.py
+++ point/view/earn/task.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from pydantic import Field
+from pydantic import Field, AnyUrl
 
 from point.entity_types import Image
 from point.view import PointBase
@@ -11,6 +11,7 @@ class TaskOut(PointBase):
     title: str = Field(max_length=128)
     description: str = Field(max_length=512)
     icon: Image
+    link: AnyUrl = Field(max_length=1024)
     profit: int = Field(gt=0)
     done: bool = Field(default=False)
 
